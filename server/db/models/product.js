fb38a331e0d0809cbabb5ee0c35e554e75b32ff1
Garrett Green <39413445+garrett-green@users.noreply.github.com>
2018-10-30 14:54:14 -0500

    Update product.js

--- server/db/models/product.js
+++ server/db/models/product.js
@@ -4,15 +4,25 @@ const db = require('../db')
 const Product = db.define('product', {
   name: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
   },
   description: {
     type: Sequelize.TEXT,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
   },
   price: {
     type: Sequelize.DECIMAL(10,2),
-    allowNull: false
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      min: 0.00
+    }
   },
   imageUrl: {
     type: Sequelize.STRING,
